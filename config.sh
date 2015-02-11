84f8e63c8e3d5faf28279ae586458dd4a13bf526
John Huddleston <jlhudd@uw.edu>
2015-02-11 14:10:11 -0800

    load configs from the correct path.

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-. genes/config.sh
-. liftover/config.sh
-. segmental_duplications/config.sh
-. sequences/config.sh
+. rules/genes/config.sh
+. rules/liftover/config.sh
+. rules/segmental_duplications/config.sh
+. rules/sequences/config.sh
