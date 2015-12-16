d31f6b94647f4091a1f6c111f694ab12178656dd
John Huddleston <jlhudd@uw.edu>
2015-12-16 11:34:47 -0800

    added path for TRF to PATH.

--- config.sh
+++ config.sh
@@ -1,7 +1,9 @@
-module load anaconda/2.1.0
+module load anaconda/2.3.0
 
 . rules/genes/config.sh
 . rules/liftover/config.sh
 . rules/segmental_duplications/config.sh
 . rules/sequences/config.sh
 . rules/tracks/config.sh
+
+export PATH=$PATH:/net/eichler/vol2/local/bin
