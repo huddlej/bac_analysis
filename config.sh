0a5ba0012128f11e6c7153ce71d2897f8c98ce96
John Huddleston <jlhudd@uw.edu>
2016-08-12 15:11:08 -0700

    added rules to align clones with BLASR.

--- config.sh
+++ config.sh
@@ -2,6 +2,7 @@ module load anaconda/2.3.0
 
 . rules/genes/config.sh
 . rules/liftover/config.sh
+. rules/align_with_blasr/config.sh
 . rules/segmental_duplications/config.sh
 . rules/sequences/config.sh
 . rules/tracks/config.sh
